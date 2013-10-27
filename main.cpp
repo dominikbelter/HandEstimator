cd6518d3991539986388cdac1510ad2a17528c83
dominikbelter <fffdd@fff.pl>
2013-10-27 23:25:52 +0100

    math library added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "handest_defs.h"
 #include "Grabber/kinect_grabber.h"
+#include "Core/Math/CMat44.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 using namespace std;
 
@@ -10,7 +13,7 @@ int main()
         using namespace handest;
 
         Grabber* grabber = createGrabberKinect();
-        cout << "Current grabber: " << grabber->getName();
+        cout << "Current grabber: " << grabber->getName() << endl;
         Point3D::Cloud scene;
         grabber->grab();
         grabber->getCloud(scene);
@@ -26,6 +29,12 @@ int main()
         //optimization->optimize(hand_cloud, hand);
         //optimization->save2File(hand);
         //visualizer->showHand(hand);
+
+        CMat44 matrix1;
+        matrix1.createTRMatrix(0, M_PI/2, 0, 0.1, 0.2, 0.3);
+        matrix1.showMatrix();
+        matrix1.inv(&matrix1);
+        matrix1.showMatrix();
     }
 	catch (const std::exception& ex) {
 		std::cerr << ex.what() << std::endl;
