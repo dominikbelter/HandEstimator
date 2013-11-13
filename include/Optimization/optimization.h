dbc333427d61648e778480b7d0b75d030fb70aa2
sebassa <sebastian.bromberek@gmail.com>
2013-11-13 22:56:52 +0100

    Header of Optimization
    Header of OptimizationPSO has been addedd.

---
+++ include/Optimization/optimization.h
@@ -0,0 +1,29 @@
+/** @file optimization.h
+ *
+ * Optimization interface
+ *
+ */
+
+#ifndef _OPTIMIZATION_H_
+#define _OPTIMIZATION_H_
+
+#include "../handest_defs.h"
+
+
+namespace handest {
+	/// Optimization interface
+	class OptimizationPSO {
+	public:
+
+		/// Returns the current point cloud
+		virtual void Optimization(Hand::Pose& hand, Point3D::Cloud& cloud) = 0;
+
+		/// Save data to file
+		virtual void SaveToFile(Hand::Pose& hand) = 0;
+
+		/// Virtual descrutor
+		virtual ~OptimizationPSO() {}
+	};
+};
+
+#endif // _OPTIMIZATION_H_
