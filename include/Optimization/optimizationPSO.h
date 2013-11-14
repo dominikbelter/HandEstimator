7483aa59ac58349b08ee70f60f6c12405b776cab
MieszkoKrajda <mieszko.krajda@gmail.com>
2013-11-14 16:45:12 +0100

    OptimizationPSO

---
+++ include/Optimization/optimizationPSO.h
@@ -0,0 +1,21 @@
+/** @file optimizationPSO.h
+ *
+ * implementation - OptimizationPSO
+ *
+ */
+
+#ifndef OPTIMIZATIONPSO_H_INCLUDED
+#define OPTIMIZATIONPSO_H_INCLUDED
+
+#include "optimization.h"
+#include <iostream>
+#include <memory>
+
+using namespace handest;
+
+class OptimizationPSO : public Optimization {
+
+
+};
+
+#endif // KINECT_GRABBER_H_INCLUDED
