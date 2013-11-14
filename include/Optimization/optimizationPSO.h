435b329a35c598cecfb56c31d0202384f543b4a4
sebassa <sebastian.bromberek@gmail.com>
2013-11-14 23:11:47 +0100

    OptimizationPSO Update

--- include/Optimization/optimizationPSO.h
+++ include/Optimization/optimizationPSO.h
@@ -11,11 +11,29 @@
 #include <iostream>
 #include <memory>
 
+namespace handest {
+	/// create a single optimization
+	Optimization* createOptimization(void);
+};
+
 using namespace handest;
 
 class OptimizationPSO : public Optimization {
+	
+	public:
+	/// Pointer
+	typedef std::unique_ptr<OptimizationPSO> Ptr;
+
+	/// Construction
+	OptimizationPSO(void);
+
+	/// Optimization with Particle Swarm Optimization Algorithm
+	virtual void Optimize(Hand::Pose& hand, Point3D::Cloud& cloud);
+
+	/// Save data to file
+	virtual void SaveToFile(Hand::Pose& hand);
 
 
 };
 
-#endif // KINECT_GRABBER_H_INCLUDED
+#endif 
--- src/Optimization/optimizationPSO.cpp
+++ src/Optimization/optimizationPSO.cpp
@@ -1,1 +1,16 @@
-#include "../include/Optimization/optimization.h"
+#include "../include/Optimization/optimizationPSO.h"
+
+using namespace handest;
+
+void OptimizationPSO::Optimize(Hand::Pose& hand, Point3D::Cloud& cloud)
+{
+	
+}
+
+void OptimizationPSO::SaveToFile(Hand::Pose& hand)
+{
+	
+}
+
+
+	
