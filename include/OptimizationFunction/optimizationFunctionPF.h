0eef300e92f1a8e7b873777e4d9817e9bd7defd7
macand <maciejandralojc@gmail.com>
2013-11-28 16:01:37 +0100

    Optimization Function Point Fitting Update

--- include/OptimizationFunction/optimizationFunctionPF.h
+++ include/OptimizationFunction/optimizationFunctionPF.h
@@ -21,7 +21,35 @@ using namespace handest;
         /// Fitness between model and grabbed cloud
         virtual float_t FitnessValue(Hand::Pose& hand,Point3D::Cloud& cloud) = 0;
 
+	private:
+		/// initial assignment
+		/// each point from cloud is assigned to the nearest point in hand
+		void initialAssignment(Hand::Pose& hand,Point3D::Cloud& cloud);
 
+		/// find the next nearest point in hand for the given point
+		int findNextNearestPoint(Hand::Pose& hand,Point3D point);
+
+		///
+		bool checkForConflictHandPoints();
+		void assignPointToHandPoint(Point3D cloudPoint,Point3D handPoint);
+		void planAssignmentChange();
+
+		float_t calculateFitnessValue();
+
+		float_t distanceBetweenPoints(Point3D point1,Point3D point2);
+
+		int handPointsCount;
+		int cloudPointsCount;
+
+		/// list of cloud points assigned to a particular hand point
+		/// no element for no assignment
+		std::vector<std::vector<int>> assignmentList;
+
+		/// list of assigned hand points (previously and currently) for a particular cloud point
+		std::vector<std::vector<int>> assignmentHistory;
+
+		/// list of distances between cloud points and current assigned hand points
+		std::vector<float_t> distances;
 	};
 
 #endif // _OPTIMIZATION_FUNCTION_GAUSS_H_
--- src/OptimizationFunction/optimizationFunctionPF.cpp
+++ src/OptimizationFunction/optimizationFunctionPF.cpp
@@ -3,5 +3,83 @@
 
 float_t optimizationFunctionPF::FitnessValue(Hand::Pose& hand,Point3D::Cloud& cloud)
 {
+	//cloud[0].position.x;
+	//hand.palm.surface[0].position.x;
+	handPointsCount=hand.palm.surface.size();
+	cloudPointsCount=cloud.size();
+
+	assignmentList.resize(handPointsCount);
+	assignmentHistory.resize(cloudPointsCount);
+	distances.resize(cloudPointsCount);
+
+	initialAssignment(hand,cloud);
+
+	//findNextNearestPoint(hand,cloud[0]);
+
+	float_t fitnessValue=calculateFitnessValue();
+	return fitnessValue;
+}
+
+void optimizationFunctionPF::initialAssignment(Hand::Pose& hand,Point3D::Cloud& cloud)
+{
+	for(int cloudPoint=0;cloudPoint<cloudPointsCount;cloudPoint++)	
+	{
+		int nearestHandPoint=findNextNearestPoint(hand,cloud[cloudPoint]);
+		assignPointToHandPoint(cloud[cloudPoint],hand.palm.surface[nearestHandPoint]);
+	}
+}
+
+bool optimizationFunctionPF::checkForConflictHandPoints()
+{
+
+	return true;
+}
+		
+void optimizationFunctionPF::assignPointToHandPoint(Point3D cloudPoint,Point3D handPoint)
+{
+
+}
+
+void optimizationFunctionPF::planAssignmentChange()
+{
+
+}
+
+int optimizationFunctionPF::findNextNearestPoint(Hand::Pose& hand,Point3D point)
+{
+	int nearestPoint;
+	float_t distance;
+	float_t nearestDistance;
+
+	for(int i=0;i<handPointsCount;i++)
+	{
+		distance=distanceBetweenPoints(hand.palm.surface[i],point);
+		if((i==0)||(distance<nearestDistance))
+		{
+			nearestDistance=distance;
+			nearestPoint=i;
+		}
+	}
 	return 0;
+}
+
+float_t optimizationFunctionPF::calculateFitnessValue()
+{
+	float_t value=0;
+	for(int currentPoint=0;currentPoint<cloudPointsCount;currentPoint++)
+	{
+		value+=distances[currentPoint];
+	}
+	return value;
+}
+
+float_t optimizationFunctionPF::distanceBetweenPoints(Point3D point1,Point3D point2)
+{
+	float_t Xsq=(point2.position.x-point1.position.x)*(point2.position.x-point1.position.x);
+	float_t Ysq=(point2.position.y-point1.position.y)*(point2.position.y-point1.position.y);
+	float_t Zsq=(point2.position.z-point1.position.z)*(point2.position.z-point1.position.z);
+
+	float_t distance=sqrt(Xsq+Ysq+Zsq);
+
+	return distance;
 }
