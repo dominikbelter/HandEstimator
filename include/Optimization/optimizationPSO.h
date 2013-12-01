116788208c71e1af38ec05c7a5f7e5dd52eccf82
sebassa <sebastian.bromberek@gmail.com>
2013-12-02 00:38:43 +0100

    Optimization PSO update

--- include/Optimization/optimizationPSO.h
+++ include/Optimization/optimizationPSO.h
@@ -18,6 +18,28 @@ namespace handest {
 
 using namespace handest;
 
+	
+/// dimension of the problem
+const int MAX_INPUTS = 10; 
+/// numbers of particles
+const int MAX_PARTICLES = 500;
+/// maximum velocity allowed
+const float_t V_MAX = 1.5 ; 
+/// number of algorithm iterations
+const int MAX_EPOCHS = 1000;
+/// range of the initial positions 
+const float_t START_RANGE_MIN_POS = -100.0;
+const float_t START_RANGE_MAX_POS = 100.0;
+/// range of the initial velocities 
+const float_t START_RANGE_MIN_VEL = -5.0;
+const float_t START_RANGE_MAX_VEL = 5.0;
+/// PSO equations parameters
+const int INERTIA = 1;
+const int C1 = 2;
+const int C2 = 2;
+
+
+
 class OptimizationPSO : public Optimization {
 	
 	public:
@@ -34,6 +56,58 @@ class OptimizationPSO : public Optimization {
 	virtual void SaveToFile(Hand::Pose& hand);
 
 
+	private:
+		
+	class cParticle {
+
+private:
+    /// value of each optimized variable
+    float_t Position[MAX_INPUTS];
+    /// the optimum value of function for particle
+    float_t PersBest;
+    /// value of particle velocity
+    float_t Velocity[MAX_INPUTS];
+    float_t BestPosition[MAX_INPUTS];
+
+public:
+    cParticle();
+    /// return particle coordiantes
+    float_t getPosition(int index) const;
+    /// set new particle coordiantes
+	void  setPosition(int index, float_t value);
+    /// return particle optimum
+    float_t getPersBest() const;
+    /// set particle optimum
+	void  setPersBest(float_t value);
+    /// return particle velocity vector
+    float_t getVelocity(int index) const;
+    /// set particle velocity vector
+	void  setVelocity(int index, float_t value);
+    /// set particle best coordinates
+	void  setBestPosition(int index, float_t value);
+    /// return particle best coordinates
+	float_t getBestPosition(int index) const;
+
+};
+
+	cParticle particles[MAX_PARTICLES];
+
+	/// maim algorithm loop
+	void PsoAlgorithm();
+	/// update velocity vector according to Kennedy&Eberhart PSO equations
+	void UpdateVelocity(int gBestIndex);
+	/// update position vector according to Kennedy&Eberhart PSO equations
+	void UpdatePositions(int gBestIndex);
+	/// initialize particle
+	void InitializeParticles();
+	/// return value of problem function 
+	float_t GetFunctionValue(int index);
+	/// return index of particle with current optimum
+	int GetMinimum();
+		/// get random numbers for PSO equations
+	float_t GetRand();
+	/// get random numbers from given range for initializing particles
+	float_t GetRandomNumber(float_t LowBound, float_t UpBound);
 };
 
 #endif 
--- src/Optimization/optimizationPSO.cpp
+++ src/Optimization/optimizationPSO.cpp
@@ -13,4 +13,134 @@ void OptimizationPSO::SaveToFile(Hand::Pose& hand)
 }
 
 
-	
+
+void OptimizationPSO::PsoAlgorithm()
+{
+    int GlobalBest = 0;
+    int Epochs = 0;
+
+	InitializeParticles();
+
+    
+        while (Epochs < MAX_EPOCHS)
+        {
+            GlobalBest = GetMinimum();
+            UpdateVelocity(GlobalBest);
+            UpdatePositions(GlobalBest);
+
+            Epochs += 1;
+        }
+       
+}
+
+void OptimizationPSO::InitializeParticles()
+{
+    float_t InitBestValue;
+    float_t InitPos;
+
+    for (int i = 0; i < MAX_PARTICLES; i++)
+    {
+        InitBestValue = 0;
+        for (int j = 0; j < MAX_INPUTS; j++)
+        {
+            particles[i].setPosition(j, GetRandomNumber(START_RANGE_MIN_POS, START_RANGE_MAX_POS));
+            InitPos = GetRandomNumber(START_RANGE_MIN_VEL, START_RANGE_MAX_VEL);
+            particles[i].setVelocity(j,InitPos);
+            particles[i].setBestPosition(j, InitPos);
+        }
+
+        InitBestValue = GetFunctionValue(i);
+        particles[i].setPersBest(InitBestValue);
+    }
+
+    return;
+}
+
+
+
+int OptimizationPSO::GetMinimum()
+{
+    int bestParticle = 0;
+
+        for (int i = 1; i < MAX_PARTICLES ; i++)
+        {
+            if (particles[i].getPersBest() < particles[bestParticle].getPersBest())
+
+                        bestParticle = i;
+
+        }
+
+return bestParticle;
+}
+
+void OptimizationPSO::UpdateVelocity(int gBestIndex)
+{
+    float_t NewVelocity[MAX_INPUTS];
+    float_t CurrentPos;
+
+    for (int i = 0; i < MAX_PARTICLES; i++)
+    {
+        for (int j = 0; j < MAX_INPUTS; j++)
+        {
+            NewVelocity[j] = INERTIA*(particles[i].getVelocity(j) + C1*GetRand()*(particles[i].getBestPosition(j) - 
+				particles[i].getPosition(j))+C2*GetRand()*(particles[gBestIndex].getBestPosition(j) - particles[i].getPosition(j)));
+
+        /// check velocity constraints
+		if (NewVelocity[j] > V_MAX)
+            particles[i].setVelocity(j,V_MAX);
+        else if (NewVelocity[j] < -V_MAX)
+            particles[i].setVelocity(j,-V_MAX);
+        else
+            particles[i].setVelocity(j, NewVelocity[j]);
+        }
+
+    }
+}
+
+void OptimizationPSO::UpdatePositions(int gBestIndex)
+{
+    float_t  NewFunctValue, tempData;
+
+    for (int i = 0; i < MAX_PARTICLES; i++)
+    {
+        for (int j = 0; j < MAX_INPUTS; j++)
+        {
+            if (i != gBestIndex)
+            {
+                tempData = particles[i].getPosition(j);
+                particles[i].setPosition(j, tempData + (particles[i].getVelocity(j)));
+            }
+        }
+
+        NewFunctValue = GetFunctionValue(i);
+
+		/// update personal best value if neccesary
+        if (NewFunctValue < particles[i].getPersBest())
+        {
+            particles[i].setPersBest(NewFunctValue);
+        }
+
+    }
+}
+
+float_t OptimizationPSO::GetFunctionValue(int index)
+{
+    float_t result;
+
+    return result;
+}
+
+float_t OptimizationPSO::GetRandomNumber(float_t LowBound, float_t UpBound)
+{
+    float_t temp;
+    temp = (float_t)LowBound + float_t(((UpBound-LowBound))*rand()/(RAND_MAX + 1.0));
+
+    return temp;
+}
+
+float_t OptimizationPSO::GetRand()
+{
+    float_t temp;
+    temp = float_t(rand()/(RAND_MAX + 1.0));
+    return temp;
+}
