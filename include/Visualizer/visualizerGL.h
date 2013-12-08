271ff898d303ed4cb817867bd87fbf5c5e7ad424
dominikbelter <fffdd@fff.pl>
2013-12-08 23:30:17 +0100

    visualizer

--- include/Visualizer/visualizerGL.h
+++ include/Visualizer/visualizerGL.h
@@ -1,33 +1,71 @@
-/** @file visualizer.h
+/** @file visualizerGL.h
  *
- * Point Cloud Visualizer interface
+ * implementation - OpenGL Visualizer
  *
  */
 
-#ifndef _VISUALIZER_H_
-#define _VISUALIZER_H_
+#ifndef VISUALIZERGL_H_INCLUDED
+#define VISUALIZERGL_H_INCLUDED
 
-#include "../handest_defs.h"
-#include <string>
-#include <vector>
+#include "visualizer.h"
+#include <iostream>
+#include <memory>
+#include <gl\glut.h>
 
 namespace handest {
-	/// Visualizer interface
-	class Visualizer {
-	public:
-
-		///Add Points
-		virtual void addCloud(Point3D::Cloud& cloud, RGBA& colour) = 0;
-
-		///Show Points
-		virtual void show() const = 0;
-
-		///Clear Points
-		virtual void clear() = 0;
-
-		/// Virtual descrutor
-		virtual ~Visualizer() {}
-	};
+	/// create a single visualizer (OpenGL)
+	Visualizer* createVisualizerGL(void);
 };
 
-#endif // _VISUALIZER_H_
+using namespace handest;
+
+/// Visualizer implementation
+class VisualizerGL : public Visualizer {
+private:
+	Point3D::Cloud myPointCloud;
+	GLdouble eyex;
+	GLdouble eyey;
+	GLdouble eyez;
+	GLdouble centerx;
+	GLdouble centery;
+	GLdouble centerz;
+	GLdouble currentx;
+	GLdouble currenty;
+	GLdouble transx;
+	GLdouble transy;
+	bool keydown;
+	void CreateHand();
+	void DrawGlobalAxis();
+	void DrawGrid();
+	enum eKey
+	{
+		Right,
+		Left
+	};
+	int currentKey;
+
+public:
+	/// Draw function for glut
+	static void Draw();
+	static void Reshape(int width, int height);
+	static void Keyboard(unsigned char key, int x, int y);
+	static void MouseClick(int button, int state,int x, int y);
+	static void MouseMove(int x, int y);
+	/// Pointer
+	typedef std::unique_ptr<VisualizerGL> Ptr;
+
+	/// Construction
+	VisualizerGL(void);
+
+	///Add Points
+	virtual void addCloud(Point3D::Cloud& cloud, RGBA& colour) ;
+
+	///Show Points
+	virtual void show() const;
+
+	///Clear Points
+	virtual void clear();
+
+};
+
+#endif // VISUALIZERGL_H_INCLUDED
